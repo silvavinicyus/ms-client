518904a83fde04c8ec7eabfd31a1cc32b0d563ad
Vinicyus <vinicyus346@gmail.com>
2022-02-21 22:43:05 -0300

    fix: create users working with kafka

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import { Kafka } from 'kafkajs';
 import 'reflect-metadata';
 import CreateUserController from '../../../modules/users/useCases/CreateUserUseCase/CreateUserController';
-import kafka from '../../../modules/utils/kafka';
+// import kafka from '../../../modules/utils/kafka';
 import createConnection from '../../../shared/infra/typeorm';
 import '../../container';
-import { AppError } from '../../errors/AppError';
+// import { AppError } from '../../errors/AppError';
 import router from './routes';
 
 createConnection().then(() => {
@@ -18,53 +19,54 @@ app.use(express.json());
 
 app.use(router);
 
-app.use( (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({ message: err.message });
-    }
-
-    return response.status(500).json({
-      status: 'error',
-      message: `Internal server error - ${err.message}`,
-    });
-  }
-);
-
-// const topicNormalUser = 'createuser';
-// const consumerNormalUser = kafka.consumer({ groupId: 'create-user' });
-// const creteUserController = new CreateUserController();
-
-// const producerUserCreated = kafka.producer();
-
-// async function createUser() {
-//   await consumerNormalUser.connect();
-//   await consumerNormalUser.subscribe({ topic: topicNormalUser, fromBeginning: false});    
-//   await producerUserCreated.connect();
-
-//   await consumerNormalUser.run({
-//     eachMessage: async ({topic, partition, message}) => {      
-//       const data = message.value.toString();
-//       const dataJson = JSON.parse(data);
-
-//       const result = await creteUserController.handle(dataJson);     
-
-//       if(!result.error) {
-//         console.log("Entrou na que não tem erro!");
-//         await producerUserCreated.send({
-//           topic: 'usercreated',
-//           messages: [{value: JSON.stringify(result)}]
-//         });      
-//         producerUserCreated.disconnect();
-//       } else {
-//         console.log('\n\n\n\n');
-//         console.log("entrou na que tem erro");
-//         console.log(result);
-//       }      
+// app.use( (err: Error, request: Request, response: Response, next: NextFunction) => {
+//     if (err instanceof AppError) {
+//       return response.status(err.statusCode).json({ message: err.message });
 //     }
-//   });  
-// }
 
-// createUser();
+//     return response.status(500).json({
+//       status: 'error',
+//       message: `Internal server error - ${err.message}`,
+//     });
+//   }
+// );
+
+const kafka = new Kafka({
+  clientId: 'ms-emails',
+  brokers: ['kafka:29092']
+});
+
+const topicNormalUser = 'createuser';
+const consumerNormalUser = kafka.consumer({ groupId: 'create-user' });
+const creteUserController = new CreateUserController();
+
+const producerUserCreated = kafka.producer();
+
+async function createUser() {
+  await consumerNormalUser.connect();
+  await consumerNormalUser.subscribe({ topic: topicNormalUser, fromBeginning: false});    
+  
+  await consumerNormalUser.run({
+    eachMessage: async ({topic, partition, message}) => {  
+      const data = message.value.toString();
+      const dataJson = JSON.parse(data);            
+      console.log({topic, partition, dataJson});      
+      const result = await creteUserController.handle(dataJson);
+      
+      await producerUserCreated.connect();
+      await producerUserCreated.send({
+        topic: 'usercreated',
+        messages: [{value: JSON.stringify(result)}]
+      });                               
+    }    
+  });  
+  
+  // producerUserCreated.disconnect();
+}
+
+createUser().then(() => {
+  console.log("CONSUMER ESTÁ FUNCIONANDO!");
+});
 
 
 app.listen(3000, () => {
